16cf047c53d20f7c39a0d306ee40d036dab82869
Rob Kennedy <rob.kennedy@videon-central.com>
2018-05-31 08:58:51 -0400

    Make use of the GPIOs

--- samplewebserver.py
+++ samplewebserver.py
@@ -1,10 +1,14 @@
-import http.server
-import socketserver
+import SimpleHTTPServer
+import SocketServer
 import json
+import RPi.GPIO as GPIO
+from random import randint
+from time import sleep
+
 
 PORT = 8000
 
-class SAMHandler(http.server.SimpleHTTPRequestHandler):
+class SAMHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def do_POST(self):
         self.send_response(200)
@@ -14,15 +18,71 @@ class SAMHandler(http.server.SimpleHTTPRequestHandler):
         self.data_string = self.rfile.read(int(self.headers['Content-Length']))
 
         data = json.loads(self.data_string)
-        self.toggle_light(data["light"])
+        if data["type"] == "light":
+            self.toggle_light(data["light"], data["status"])
+        elif data["type"] == "servo":
+            self.feedMe()
 
-    def toggle_light(self, light_number):
+    def toggle_light(self, light_type, light_status):
         print(("Turning on light number {}".format(light_number)))
+        if light_type == "day":
+            if light_status == "on":
+                self.toggleOn(31)
+            elif light_status == "off":
+                self.toggleOff(31)
+        elif light_type == "night":
+            if light_status == "on":
+                self.toggleOn(29)
+            elif light_status == "off":
+                self.toggleOff(29)
+ 
+    def toggleOn(self, gpio_pin_number):
+        GPIO.setup(gpio_pin_number, GPIO.OUT)
 
+        print ("Turn Relay ON")
+        GPIO.output(gpio_pin_number, GPIO.LOW)
+
+    def toggleOff(self, gpio_pin_number):
+        GPIO.setup(gpio_pin_number, GPIO.OUT)
+
+        print ("Turn Relay OFF")
+        GPIO.output(gpio_pin_number, GPIO.HIGH)
+    
+    def feedMe(self):
+        self.servo(0, 3)
+        self.servo(0, 11)
+        self.servo(0, 13)
+        self.servo(0, 15)
+        self.servo(180, 3)
+        self.servo(180, 11)
+        self.servo(180, 13)
+        self.servo(180, 15)
+
+    def servo(self, angle, number):
+        pause = 1
+        GPIO.setup(number, GPIO.OUT)
+        pwm=GPIO.PWM(number, 50)
+        pwm.start(0)
+        duty = angle / 18 + 2
+        GPIO.output(number, True)
+        pwm.ChangeDutyCycle(duty)
+        sleep(0.5)
+        GPIO.output(number, False)
+        pwm.ChangeDutyCycle(0)
+    
+
+# Run this all everytime.
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(03, GPIO.OUT)
+pwm=GPIO.PWM(03, 50)
+pwm.start(0)
 
 Handler = SAMHandler
 
-httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 print("serving at port {}".format(PORT))
 httpd.serve_forever()
+
+GPIO.cleanup()
